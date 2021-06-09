ec5e00c9795c981694b6d36ca28af3419742f83c
Simone Odegard <Simone.Odegard@gmail.com>
2021-06-09 15:48:08 -0700

    added styling to cart

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <>
       <Header />
-      <Categories />
       <Cart />
+      <Categories />
       <Products />
       <Footer />
     </>
--- src/components/simple-cart.js
+++ src/components/simple-cart.js
@@ -1,22 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
 import { activeCat, reset } from '../store/products.js';
 import { increment, add } from '../store/simple-cart.js';
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 250,
+    position: 'fixed',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 function SimpleCart(props) {
+
+  const classes = useStyles();
+
   return (
     <>
-      <h3>Cart</h3>
-      <div>
-        {props.cartReducer.cartItems.map(product => {
-          return (
-            <ul>
-              Name: {product.name}, Price: {product.price}
-            </ul>
-          )
-        })}
-      </div>
+      <Card id="cartCard" className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Cart
+          </Typography>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+          </Typography>
+          <div>
+            {props.cartReducer.cartItems.map(product => {
+              return (
+                <>
+                  <Typography>
+                    {product.name}
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    {product.price}
+                  </Typography>
+                  <CardActions>
+                    <Button size="small">Remove</Button>
+                  </CardActions>
+                </>
+              )
+            })}
+          </div>
+        </CardContent>
+      </Card>
     </>
   )
 }
